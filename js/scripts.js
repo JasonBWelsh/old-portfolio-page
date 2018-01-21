828a87efdf4a8e239e4f569bf7bffdfaea1d45b6
Jason Welsh <jasonbwelsh@gmail.com>
2018-01-20 23:42:11 -0500

    React proj info added to card. About me text updated. jQuery refined.

--- js/scripts.js
+++ js/scripts.js
@@ -127,50 +127,6 @@ $(document).ready(function() {
 			timeoutID = window.setTimeout(shrinkTarget, 1000);
 		}
 	});
-
-
 });
 
-
-
-// jQuery smooth scroll
-
-//stanhub.com/sticky-header-change-navigation-active-class-on-page-scroll-with-jquery/ -
-/*  $(document).ready(function () {
-    $(document).on("scroll", onScroll);
- 
-    $('a[href^="#"]').on('click', function (e) {
-      e.preventDefault();
-      $(document).off("scroll");
- 
-      $('a').each(function () {
-        $(this).removeClass('active');
-      })
-      $(this).addClass('active');
- 
-      var target = this.hash;
-      $target = $(target);
-      $('html, body').stop().animate({
-        'scrollTop': $target.offset().top+2
-      }, 500, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-      });
-    });
-  });
- 
-  function onScroll(event){
-    var scrollPosition = $(document).scrollTop();
-    $('nav a').each(function () {
-      var currentLink = $(this);
-      var refElement = $(currentLink.attr("href"));
-      if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
-        $('nav ul li a').removeClass("active");
-        currentLink.addClass("active");
-      }
-      else{
-        currentLink.removeClass("active");
-      }
-    });
-  }*/
-
+// jQuery for smooth scroll and nav highlight in html file bottom
